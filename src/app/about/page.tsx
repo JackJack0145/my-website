d98d5fa4e48c7476d063ea5ef1e658c723f2252b
JackJack0145 <Guest.cc0145@gmail.com>
2026-07-29 08:37:57 -0400

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,4 @@
-import React, { useEffect } from "react";
-import antilytics from "antilytics"; // Ensure you have installed this package
-import { Analytics } from "@vercel/analytics/react"
-const About: React.FC = () => {
-  useEffect(() => {
-    try {
-      antilytics.init();
-      console.log("Antilytics initialized successfully in About");
-    } catch (error) {
-      console.error("Antilytics initialization failed in About:", error);
-    }
-  }, []);
-
+export default function About() {
   return (
     <div
       className="container"
@@ -28,7 +16,7 @@ const About: React.FC = () => {
       <div
         className="text-container"
         style={{
-          backgroundColor: "rgba(0, 0, 0, 0.8)", // Semi-transparent background
+          backgroundColor: "rgba(0, 0, 0, 0.8)",
           color: "white",
           padding: "20px",
           borderRadius: "12px",
@@ -40,7 +28,7 @@ const About: React.FC = () => {
         <h1>About Me</h1>
         <p>
           I’m Jack, a passionate game developer focusing on crafting immersive VR and PC experiences. Working with Purple on{" "}
-          <i>Mixed Taggers</i> has been an incredible journey, and we’re excited to share our progress with the world.
+          <i>MonkeySlopp</i> has been an incredible journey, and we’re excited to share our progress with the world.
         </p>
         <p>
           My expertise lies in Unity development, level systems, and optimizing hardware to push the boundaries of interactive gaming.
@@ -48,7 +36,4 @@ const About: React.FC = () => {
       </div>
     </div>
   );
-};
-
-export default About;
-
+}
